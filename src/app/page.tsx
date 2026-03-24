44c3505fe2915b11f2bbcc44a02f133dea9e4425
Nazar <nazarpalamar46@gmail.com>
2026-03-24 15:23:03 +0100

    moved href to object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 
+const href = {
+  auth: '/auth',
+};
+
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <div className='flex min-h-screen w-full max-w-3xl items-center gap-x-6 bg-white px-16 py-32 sm:items-start dark:bg-black'>
-        <Link href={'/auth'}>Auth</Link>
+        <Link href={href.auth}>Auth</Link>
       </div>
     </main>
   );
