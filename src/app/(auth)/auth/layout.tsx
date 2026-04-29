d91a2bff29325defd2205cb02bf2cd6657054fec
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-04-29 22:26:12 +0300

    feat: added spinner to suspence & redirect ro main page in page logo

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,12 +1,22 @@
 import { Suspense } from 'react';
 
+import { Spinner } from '@/shared/ui/spinner';
+
 const AuthLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => (
   <main className='flex h-full flex-col items-center justify-center'>
-    <Suspense fallback={<h1>Loading...</h1>}>{children}</Suspense>
+    <Suspense
+      fallback={
+        <div className='flex items-center justify-center'>
+          <Spinner className='size-6' />
+        </div>
+      }
+    >
+      {children}
+    </Suspense>
   </main>
 );
 
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -9,7 +9,9 @@ import { Button } from '@/shared/ui/button';
 const Header = () => (
   <header className='bg-card text-card-foreground border-border left-0 flex h-15 items-center justify-between gap-10 border-b px-10 py-5 shadow-sm'>
     <div className='flex items-center gap-15'>
-      <h3 className='typo-h3'>ExactHunter</h3>
+      <Link href={AppRouter.main} className='typo-h3'>
+        ExactHunter
+      </Link>
       <HeaderNav />
     </div>
 
