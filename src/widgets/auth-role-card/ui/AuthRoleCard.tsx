9b51d9ee22092eb9be825541615cd946572ef557
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-05-01 00:40:09 +0300

    feat: added BackButton to auth page

--- src/widgets/auth-role-card/ui/AuthRoleCard.tsx
+++ src/widgets/auth-role-card/ui/AuthRoleCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
+import { useRouter } from 'next/navigation';
 import { ChartSpline } from 'lucide-react';
 
 import { NavigateButtons } from './components/navigate-buttons/NavigateButtons';
@@ -8,16 +9,28 @@ import { AuthRoleCardProps } from './AuthRoleCard.types';
 
 import { RoleForm } from '@/features/auth';
 
+import { BackButton } from '@/shared/ui/back-button';
+
 export const AuthRoleCard = ({ role }: AuthRoleCardProps) => {
+  const router = useRouter();
   const selectRoleFormRef = useRef<HTMLFormElement | null>(null);
 
   return (
     <div className='mx-auto grid w-full max-w-125 gap-y-6.25 px-5 sm:px-0'>
-      <div className='flex flex-col items-center justify-center space-y-2.5'>
-        <div className='bg-blue-35 w-fit rounded-[1.875rem] p-3.5'>
-          <ChartSpline aria-label='Logo icon' color='white' size={72} />
+      <div className='relative flex justify-center'>
+        <BackButton
+          size='icon-lg'
+          className='absolute top-0 left-0'
+          onBack={() => {
+            router.back();
+          }}
+        />
+        <div className='flex flex-col items-center justify-center space-y-2.5'>
+          <div className='bg-blue-35 w-fit rounded-[1.875rem] p-3.5'>
+            <ChartSpline aria-label='Logo icon' color='white' size={72} />
+          </div>
+          <h1 className='text-center text-3xl font-bold'>ExactHunter</h1>
         </div>
-        <h1 className='text-center text-3xl font-bold'>ExactHunter</h1>
       </div>
       <RoleForm role={role} ref={selectRoleFormRef} />
 
