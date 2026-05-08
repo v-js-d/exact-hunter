4557081e0f257ef101a367a5640fa41a65a1c103
kirilldolzhenkov <k.dolzhenkov@gmail.com>
2026-05-08 13:53:39 +0300

    refactor: removed AuthErrorResponse

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,7 +1,7 @@
 export { isAuthMode } from './model/guards/isAuthMode';
 export { useLoginMutation } from './model/hooks/useLoginMutation';
 export { useRegisterMutation } from './model/hooks/useRegisterMutation';
-export type { AuthErrorResponse, AuthMode } from './model/types/auth.types';
+export type { AuthMode } from './model/types/auth.types';
 export type { AuthMethod, authMethods } from './model/types/auth-method.types';
 export { AuthForm } from './ui/auth-form/AuthForm';
 export { RoleForm } from './ui/auth-role-form/RoleForm';
